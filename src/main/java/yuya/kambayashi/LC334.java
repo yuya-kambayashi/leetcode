2fe309110b12f3ba39234d5cafbaa1631c7af200
yuya-kambayashi <kambayashi73@gmail.com>
2023-10-30 14:46:39 +0900

    solve LC334

--- src/main/java/yuya/kambayashi/LC334.java
+++ src/main/java/yuya/kambayashi/LC334.java
@@ -3,6 +3,21 @@ package yuya.kambayashi;
 public class LC334 {
     public boolean increasingTriplet(int[] nums) {
 
-        return true;
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for(int n : nums){
+            if (n <= first){
+                first = n;
+            }
+            else if (n <= second){
+                second = n;
+            }
+            else{
+                return true;
+            }
+        }
+
+        return false;
     }
 }
