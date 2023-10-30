6b6f775c41f2a1ec991cb41ea38a6e344ec3fb5b
yuya-kambayashi <kambayashi73@gmail.com>
2023-10-30 17:22:46 +0900

    solve LC392

--- src/main/java/yuya/kambayashi/LC392.java
+++ src/main/java/yuya/kambayashi/LC392.java
@@ -3,6 +3,24 @@ package yuya.kambayashi;
 public class LC392 {
     public boolean isSubsequence(String s, String t) {
 
+        int tindex = 0;
+        for(char cs : s.toCharArray()){
+            boolean find = false;
+            for(int i = tindex; i < t.length(); i++ ){
+                char ct = t.charAt(i);
+                if (cs == ct){
+                    tindex = i;
+                    tindex++;
+                    find = true;
+                    break;
+                }
+            }
+            if (!find){
+                return false;
+            }
+
+        }
+
         return true;
     }
 }
