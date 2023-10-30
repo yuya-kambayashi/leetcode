93ebfab0bc8b29bca68e300d3e9d3585a5439458
yuya-kambayashi <kambayashi73@gmail.com>
2023-10-30 14:20:13 +0900

    add LC238

---
+++ src/main/java/yuya/kambayashi/LC238.java
@@ -0,0 +1,8 @@
+package yuya.kambayashi;
+
+public class LC238 {
+    public int[] productExceptSelf(int[] nums) {
+
+        return null;
+    }
+}
---
+++ src/test/java/yuya/kambayashi/LC238Test.java
@@ -0,0 +1,22 @@
+package yuya.kambayashi;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LC238Test {
+    void doTest(int[] nums, int[] expected) {
+
+        var actual = new LC238().productExceptSelf(nums);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void Case1() {
+        doTest(new int[]{1,2,3,4}, new int[]{24,12,8,6});
+    }
+    @Test
+    void Case2() {
+        doTest(new int[]{-1,1,0,-3,3}, new int[]{0,0,9,0,0});
+    }
+}
