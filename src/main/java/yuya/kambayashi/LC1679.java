5b28e70b03451c708f8cfb2718d5a1d508c5d517
yuya-kambayashi <kambayashi73@gmail.com>
2023-11-01 17:52:01 +0900

    solve LC1679

--- src/main/java/yuya/kambayashi/LC1679.java
+++ src/main/java/yuya/kambayashi/LC1679.java
@@ -1,8 +1,32 @@
 package yuya.kambayashi;
 
+import java.util.Arrays;
+
 public class LC1679 {
     public int maxOperations(int[] nums, int k) {
 
-        return 1;
+        Arrays.sort(nums);
+
+        int count = 0;
+
+        int left = 0;
+        int right = nums.length -1;
+
+        while(left < right){
+            if (nums[left] + nums[right] < k){
+                left++;
+            }
+            else if (nums[left] + nums[right] > k){
+                right--;
+            }
+            else{
+                left++;
+                right--;
+                count++;
+            }
+        }
+
+
+        return count;
     }
 }
