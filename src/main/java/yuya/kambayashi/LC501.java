9ab64d951d75f607f5c02e01fbf0989173377b38
yuya-kambayashi <kambayashi73@gmail.com>
2023-11-01 17:22:01 +0900

    solve LC501

--- src/main/java/yuya/kambayashi/LC501.java
+++ src/main/java/yuya/kambayashi/LC501.java
@@ -1,9 +1,45 @@
 package yuya.kambayashi;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LC501 {
+
+
     public int[] findMode(TreeNode root) {
 
-        return null;
+        Map<Integer, Integer> counter = new HashMap<>();
+        dfs(root, counter);
+        int maxFreq = 0;
+
+        for(int key : counter.keySet()){
+            maxFreq = Math.max(maxFreq, counter.get(key));
+        }
+
+        List<Integer> ans = new ArrayList();
+        for(int key: counter.keySet()){
+            if (counter.get(key) == maxFreq){
+                ans.add(key);
+            }
+        }
+
+        int[] result = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++){
+            result[i] = ans.get(i);
+        }
+
+        return result;
+    }
+    public  void dfs(TreeNode node, Map<Integer, Integer> counter){
+        if (node == null){
+            return;
+        }
+        counter.put(node.val, counter.getOrDefault(node.val, 0) + 1);
+
+        dfs(node.left, counter);
+        dfs(node.right, counter);
     }
 
 
