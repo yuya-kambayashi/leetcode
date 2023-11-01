02c30be93da02d545f84f17b16db00964135e4a5
yuya-kambayashi <kambayashi73@gmail.com>
2023-11-01 17:45:09 +0900

    solve LC011

--- src/main/java/yuya/kambayashi/LC011.java
+++ src/main/java/yuya/kambayashi/LC011.java
@@ -2,7 +2,23 @@ package yuya.kambayashi;
 
 public class LC011 {
     public int maxArea(int[] height) {
+        int maxArea = Integer.MIN_VALUE;
 
-        return 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while(left < right){
+            int width = right - left;
+
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * width);
+
+            if (height[left] <= height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxArea;
     }
 }
