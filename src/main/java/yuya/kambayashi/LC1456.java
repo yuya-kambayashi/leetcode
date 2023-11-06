42572fe45d4f28b80ae70fea4d6fce21aa41115a
yuya-kambayashi <kambayashi73@gmail.com>
2023-11-06 17:42:49 +0900

    solve LC1456

--- src/main/java/yuya/kambayashi/LC1456.java
+++ src/main/java/yuya/kambayashi/LC1456.java
@@ -3,6 +3,29 @@ package yuya.kambayashi;
 public class LC1456 {
     public int maxVowels(String s, int k) {
 
-        return 2;
+        int max = 0;
+        for(int i = 0; i < k; i++){
+            if (isVowel(s.charAt(i))){
+                max++;
+            }
+        }
+
+        int res = max;
+
+        for(int i = k; i < s.length(); i++){
+            if (isVowel(s.charAt(i - k))){
+                max--;
+            }
+
+            if (isVowel(s.charAt(i))){
+                max++;
+            }
+            res = Math.max(res, max);
+        }
+
+        return res;
+    }
+    public boolean isVowel(char c){
+        return c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o';
     }
 }
