f485371b17608dcab5f152e0b5c7dd1d83c812c3
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-02-24 23:51:30 +0900

    LC2092の実装をテストのほうに移します

--- src/test/java/yuya/kambayashi/LC2092Test.java
+++ src/test/java/yuya/kambayashi/LC2092Test.java
@@ -1,15 +1,69 @@
 package yuya.kambayashi;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LC2092Test {
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson){
+
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        
+        for (int[] meeting : meetings) {
+            int x = meeting[0], y = meeting[1], t = meeting[2];
+            graph.computeIfAbsent(x, k -> new ArrayList<>()).add(new int[]{t, y});
+            graph.computeIfAbsent(y, k -> new ArrayList<>()).add(new int[]{t, x});
+        }
+
+        // Earliest time at which a person learned the secret 
+        // as per current state of knowledge. If due to some new information, 
+        // the earliest time of knowing the secret changes, we will update it
+        // and again process all the people whom he/she meets after the time
+        // at which he/she learned the secret.
+        int[] earliest = new int[n];
+        Arrays.fill(earliest, Integer.MAX_VALUE);
+        earliest[0] = 0;
+        earliest[firstPerson] = 0;
+        
+        // Queue for BFS. It will store (person, time of knowing the secret)
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
+        q.offer(new int[]{firstPerson, 0});
+
+        // Do BFS
+        while (!q.isEmpty()) {
+            int[] personTime = q.poll();
+            int person = personTime[0], time = personTime[1];
+            for (int[] nextPersonTime : graph.getOrDefault(person, new ArrayList<>())) {
+                int t = nextPersonTime[0], nextPerson = nextPersonTime[1];
+                if (t >= time && earliest[nextPerson] > t) {
+                    earliest[nextPerson] = t;
+                    q.offer(new int[]{nextPerson, t});
+                }
+            }
+        }
+        
+        // Since we visited only those people who know the secret,
+        // we need to return indices of all visited people.
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            if (earliest[i] != Integer.MAX_VALUE) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
     void doTest(int n, int[][] meetings, int firstPerson, List<Integer> expected) {
 
-        var actual = new LC2092().findAllPeople(n, meetings, firstPerson);
+        //var actual = new LC2092().findAllPeople(n, meetings, firstPerson);
+        var actual = findAllPeople(n, meetings, firstPerson);
 
         assertThat(actual).containsAnyElementsOf(expected);
         
