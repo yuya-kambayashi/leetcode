f96e9bc2caa3988132e9502be5e39cc30a741b85
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-03-10 00:06:02 +0900

    solve

--- src/test/java/yuya/kambayashi/LC2540Test.java
+++ src/test/java/yuya/kambayashi/LC2540Test.java
@@ -4,6 +4,8 @@
  */
 package yuya.kambayashi;
 
+import java.util.HashSet;
+import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
@@ -14,15 +16,44 @@ import org.junit.jupiter.api.Test;
 public class LC2540Test {
      
     public int getCommon(int[] nums1, int[] nums2) {
+  
+        Set<Integer> set1 = new HashSet<>();
+        for(int n : nums1){
+            set1.add(n);
+        }
         
-        return 2;
+        int min = Integer.MAX_VALUE;
+        for(int n : nums2){
+            if (set1.contains(n)){
+                min = Math.min(min, n);
+            }
+        }
+        
+        if (min == Integer.MAX_VALUE){
+            return -1;
+        }
+        return min;
     }
+  
 
     @Test
     void Case1(){
             
         int[] nums1 = {1,2,3};    
         int[] nums2 = {2,4};
+
+        
+        var actual = getCommon(nums1, nums2);
+        
+        var expected = 2;
+        
+        assertThat(actual).isEqualTo(expected);
+    }
+        @Test
+    void Case2(){
+            
+        int[] nums1 = {1,2,3,6};    
+        int[] nums2 = {2,3,4,5};
 
         
         var actual = getCommon(nums1, nums2);
