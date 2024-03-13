c3263523238ce0da59ae79e8ef0cf08deec067c5
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-03-13 16:54:43 +0900

    solve

--- src/test/java/yuya/kambayashi/LC2485Test.java
+++ src/test/java/yuya/kambayashi/LC2485Test.java
@@ -5,6 +5,7 @@
 package yuya.kambayashi;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,11 +14,30 @@ import org.junit.jupiter.api.Test;
  */
 public class LC2485Test {
     public int pivotInteger(int n) {
+        
+        for(int i = 1; i <= n; i++){
+            
+            // 前半の和
+            int before = 0;
+            for(int j = 1; j <= i; j++){
+                before += j;
+            }
+            
+            // 後半の和
+            int after = 0;
+            for(int j = i; j <= n; j++){
+                after += j;
+            }
+            
+            if (before == after){
+                return i;
+            }
+        }
 
         
-        return 1;
+        return -1;
     }
-
+    @Disabled
     @Test
     void Case1() {
         
@@ -26,6 +46,29 @@ public class LC2485Test {
         var actual = pivotInteger(n);
         
         var expected = 6;
+        
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void Case2() {
+        
+        int n = 1;
+        
+        var actual = pivotInteger(n);
+        
+        var expected = 1;
+        
+        assertThat(actual).isEqualTo(expected);
+    }
+        @Disabled
+    @Test
+    void Case3() {
+        
+        int n = 4;
+        
+        var actual = pivotInteger(n);
+        
+        var expected = -1;
         
         assertThat(actual).isEqualTo(expected);
     }
