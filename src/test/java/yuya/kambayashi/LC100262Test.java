fb53d9d5345d9f9b3830f490e005dfa841b45b1b
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-03-16 23:40:22 +0900

    solve

--- src/test/java/yuya/kambayashi/LC100262Test.java
+++ src/test/java/yuya/kambayashi/LC100262Test.java
@@ -5,6 +5,7 @@
 package yuya.kambayashi;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -15,7 +16,33 @@ public class LC100262Test {
     
     public int sumOfEncryptedInt(int[] nums) {
         
-        return 1;
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++){
+            int n = nums[i];
+            
+            String s = Integer.valueOf(n).toString();
+            char[] ca = s.toCharArray();
+                        
+            int max = 0;
+            for(char c : ca){
+                int nn = (int)(c - '0');
+                max = Math.max(max, nn);
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < s.length(); j++){
+                sb.append(max);
+            }
+            
+            String ss = sb.toString();
+            
+            int nv = Integer.valueOf(ss);
+               
+            ans += nv;
+            
+        }
+        
+        return ans;
     }
 
     @Test
