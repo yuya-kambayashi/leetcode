d1c48ad9f0eafeadc313efcce2c79e357a5eec0a
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-03-29 00:10:24 +0900

    solve

--- src/test/java/yuya/kambayashi/ABC085C.java
+++ src/test/java/yuya/kambayashi/ABC085C.java
@@ -6,8 +6,8 @@ package yuya.kambayashi;
 
 import java.util.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -17,71 +17,92 @@ import org.junit.jupiter.api.TestInstance;
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ABC085C {
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         final int n = sc.nextInt();
-        int[] da = new int[n];
-        for (int i = 0; i < n; i++) {
-            da[i] = sc.nextInt();
+        final int y = sc.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 0; j <= n - i; j++) {
+
+                int k = n - i - j;
+
+                if (10000 * i + 5000 * j + 1000 * k != y) {
+                    continue;
+                }
+                String ret = String.valueOf(i);
+                ret += " ";
+                ret += String.valueOf(j);
+                ret += " ";
+                ret += String.valueOf(k);
+                System.out.println(ret);
+                return;
+
+            }
         }
-        
-        System.out.println();
+
+        System.out.println("-1 -1 -1");
     }
-	
-    private StandardInputSnatcher  in = new StandardInputSnatcher();
-    private StandardOutputSnatcher  out = new StandardOutputSnatcher ();
-    
-    @BeforeAll
-    public void beforeAll(){
+
+    private StandardInputSnatcher in = new StandardInputSnatcher();
+    private StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeEach
+    public void beforeEach() {
         System.setIn(in);
         System.setOut(out);
     }
-    @AfterAll
-    public void afterAll(){
+
+    @AfterEach
+    public void afterEach() {
         System.setIn(null);
         System.setOut(null);
     }
 
     @Test
     public void Case1() {
-        
+
         in.inputln("9 45000");
-                
-        ABC081A.main(null);
+
+        ABC085C.main(null);
 
         var actual = out.readLine();
-        
-        var expected = "4 0 5"; 
-        
-        assertThat(actual).isEqualTo(expected); 
+
+        var expected = "4 0 5";
+        var expected2 = "0 9 0";
+
+        assertThat(actual).isIn(expected, expected2);
     }
+
     @Test
     public void Case2() {
-        
+
         in.inputln("20 196000");
-                
-        ABC081A.main(null);
+
+        ABC085C.main(null);
 
         var actual = out.readLine();
-        
-        var expected = "-1 -1 -1"; 
-        
-        assertThat(actual).isEqualTo(expected); 
+
+        var expected = "-1 -1 -1";
+
+        assertThat(actual).isEqualTo(expected);
     }
+
     @Test
     public void Case3() {
-        
+
         in.inputln("1000 1234000");
-                
-        ABC081A.main(null);
+
+        ABC085C.main(null);
 
         var actual = out.readLine();
-        
-        var expected = "14 27 959"; 
-        
-        assertThat(actual).isEqualTo(expected); 
+
+        var expected = "14 27 959";
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
 
