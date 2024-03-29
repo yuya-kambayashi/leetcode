d60dbab52407a85c24b6879cada5fa63be95fb0b
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-03-30 00:23:34 +0900

    solve

--- src/test/java/yuya/kambayashi/ABC086C.java
+++ src/test/java/yuya/kambayashi/ABC086C.java
@@ -8,7 +8,6 @@ import java.util.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -23,12 +22,45 @@ public class ABC086C {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
-        int[] da = new int[n];
+        int[][] naa = new int[n][3];
         for (int i = 0; i < n; i++) {
-            da[i] = sc.nextInt();
+            for (int j = 0; j < 3; j++) {
+                naa[i][j] = sc.nextInt();
+            }
         }
 
-        System.out.println();
+        int tb = 0, xb = 0, yb = 0;
+
+        for (int i = 0; i < n; i++) {
+            int[] na = naa[i];
+
+            int t = na[0];
+            int x = na[1];
+            int y = na[2];
+
+            int distance = Math.abs(x - xb) + Math.abs(y - yb);
+
+            int time = t - tb;
+
+            if (distance > time) {
+                System.out.println("No");
+                return;
+            } else if (distance < time) {
+
+                int rest = time % distance;
+
+                if (rest != 0) {
+                    System.out.println("No");
+                    return;
+                }
+            }
+
+            tb = t;
+            xb = x;
+            yb = y;
+        }
+
+        System.out.println("Yes");
     }
 
     private StandardInputSnatcher in = new StandardInputSnatcher();
@@ -46,47 +78,52 @@ public class ABC086C {
         System.setOut(null);
     }
 
-    @Disabled
+    ///@Disabled
     @Test
     public void Case1() {
 
-        in.inputln("");
+        in.inputln("2");
+        in.inputln("3 1 2");
+        in.inputln("6 1 1");
 
         ABC086C.main(null);
 
         var actual = out.readLine();
 
-        var expected = "";
+        var expected = "Yes";
 
         assertThat(actual).isEqualTo(expected);
     }
 
-    @Disabled
+    //@Disabled
     @Test
     public void Case2() {
 
-        in.inputln("");
+        in.inputln("1");
+        in.inputln("2 100 100");
 
         ABC086C.main(null);
 
         var actual = out.readLine();
 
-        var expected = "";
+        var expected = "No";
 
         assertThat(actual).isEqualTo(expected);
     }
 
-    @Disabled
+    //@Disabled
     @Test
     public void Case3() {
 
-        in.inputln("");
+        in.inputln("2");
+        in.inputln("5 1 1");
+        in.inputln("100 1 1");
 
         ABC086C.main(null);
 
         var actual = out.readLine();
 
-        var expected = "";
+        var expected = "No";
 
         assertThat(actual).isEqualTo(expected);
     }
