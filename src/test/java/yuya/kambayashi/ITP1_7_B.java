bb42a9853cc901d6790729ec090781777aab136f
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-03-30 00:35:21 +0900

    add

---
+++ src/test/java/yuya/kambayashi/ITP1_7_B.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yuya.kambayashi;
+
+import java.util.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ *
+ * @author kamba
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ITP1_7_B {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int n = sc.nextInt();
+        final int x = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+        System.out.println();
+    }
+
+    private StandardInputSnatcher in = new StandardInputSnatcher();
+    private StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeAll
+    public void beforeAll() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+
+    //@Disabled
+    @Test
+    public void Case1() {
+
+        in.inputln("5 9");
+        in.inputln("0 0");
+
+        ITP1_7_B.main(null);
+
+        var actual = out.readLine();
+
+        var expected = "";
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Disabled
+    @Test
+    public void Case2() {
+
+        in.inputln("");
+
+        ITP1_7_B.main(null);
+
+        var actual = out.readLine();
+
+        var expected = "";
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Disabled
+    @Test
+    public void Case3() {
+
+        in.inputln("");
+
+        ITP1_7_B.main(null);
+
+        var actual = out.readLine();
+
+        var expected = "";
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
+
+//import java.util.*;
+//public class Main {
