4022c367d4ff0e3bbfddfef7189a4fa3fea8d95c
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-05-06 20:36:39 +0900

    add

---
+++ src/test/java/yuya/kambayashi/ABC280C.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yuya.kambayashi;
+
+import java.util.*;
+import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ *
+ * @author kamba
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ABC280C {
+
+    private StandardInputSnatcher in = new StandardInputSnatcher();
+    private StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeAll
+    public void beforeAll() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+//import java.math.*;
+//import java.util.*;
+//import java.util.stream.*;
+//public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final String s = sc.next();
+        final String t = sc.next();
+
+        int index = 0;
+        while (index < s.length()) {
+            if (s.charAt(index) != t.charAt(index)) {
+                System.out.println(index + 1);
+                return;
+            }
+            index++;
+        }
+
+        System.out.println(t.length());
+    }
+//}
+
+    @Test
+    public void Case1() {
+
+        String input = """
+                       atcoder
+                       atcorder
+                    """;
+
+        String expected = """
+                          5
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC280C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case2() {
+
+        String input = """
+                       million
+                       milllion
+                    """;
+
+        String expected = """
+                          5
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC280C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case3() {
+
+        String input = """
+                       vvwvw
+                       vvvwvw
+                    """;
+
+        String expected = """
+                          3
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC280C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+}
---
+++ src/test/java/yuya/kambayashi/ABC302C.java
@@ -0,0 +1,181 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yuya.kambayashi;
+
+import java.util.*;
+import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ *
+ * @author kamba
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ABC302C {
+
+    private StandardInputSnatcher in = new StandardInputSnatcher();
+    private StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeAll
+    public void beforeAll() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+//import java.math.*;
+//import java.util.*;
+//import java.util.stream.*;
+//public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int n = sc.nextInt();
+        final int m = sc.nextInt();
+
+        String[] ss = new String[n];
+        for (int i = 0; i < n; i++) {
+            ss[i] = sc.next();
+        }
+
+        int[] seeds = new int[n];
+        for (int i = 0; i < n; i++) {
+            seeds[i] = i;
+        }
+        var perms = permutation(seeds);
+
+        for (var perm : perms) {
+
+            boolean ok = true;
+            String before = ss[perm.get(0)];
+            for (int i = 1; i < n; i++) {
+                int diff = 0;
+                String cur = ss[perm.get(i)];
+
+                for (int j = 0; j < m; j++) {
+                    if (before.charAt(j) != cur.charAt(j)) {
+                        diff++;
+                    }
+                }
+                if (diff != 1) {
+                    ok = false;
+                    break;
+                }
+                before = cur;
+                diff = 0;
+            }
+            if (ok) {
+                System.out.println("Yes");
+                return;
+
+            }
+
+        }
+
+        System.out.println("No");
+    }
+
+    private static ArrayList<ArrayList<Integer>> permutation(int[] seed) {
+        ArrayList<ArrayList<Integer>> array = new ArrayList<ArrayList<Integer>>();
+        int[] perm = new int[seed.length];
+        boolean[] used = new boolean[seed.length];
+        buildPerm(seed, perm, used, 0, array);
+        return array;
+    }
+
+    private static void buildPerm(int[] seed, int[] perm, boolean[] used, int index, ArrayList<ArrayList<Integer>> array) {
+        if (index == seed.length) {
+            procPerm(perm, array);
+            return;
+        }
+
+        for (int i = 0; i < seed.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            perm[index] = seed[i];
+            used[i] = true;
+            buildPerm(seed, perm, used, index + 1, array);
+            used[i] = false;
+        }
+    }
+
+    private static void procPerm(int[] perm, ArrayList<ArrayList<Integer>> array) {
+        ArrayList<Integer> k = new ArrayList<>(perm.length);
+        for (int i = 0; i < perm.length; i++) {
+            k.add(perm[i]);
+        }
+        array.add(k);
+    }
+//}
+
+    @Test
+    public void Case1() {
+
+        String input = """
+                       4 4
+                       bbed
+                       abcd
+                       abed
+                       fbed
+                    """;
+
+        String expected = """
+                          Yes
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC302C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case2() {
+
+        String input = """
+                       2 5
+                       abcde
+                       abced
+                    """;
+
+        String expected = """
+                          No
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC302C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case3() {
+
+        String input = """
+                       8 4
+                       fast
+                       face
+                       cast
+                       race
+                       fact
+                       rice
+                       nice
+                       case
+                    """;
+
+        String expected = """
+                          Yes
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC302C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+}
