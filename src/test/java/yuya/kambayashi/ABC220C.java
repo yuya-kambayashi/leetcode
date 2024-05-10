b32faeaad25fe259b3066df451ac1648cef1ca0f
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-05-10 11:45:00 +0900

    add

---
+++ src/test/java/yuya/kambayashi/ABC220C.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yuya.kambayashi;
+
+import java.util.*;
+import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ *
+ * @author kamba
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ABC220C {
+
+    private StandardInputSnatcher in = new StandardInputSnatcher();
+    private StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeAll
+    public void beforeAll() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+//import java.math.*;
+//import java.util.*;
+//import java.util.stream.*;
+//public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int n = sc.nextInt();
+        int[] aa = new int[n];
+        long sum1 = 0;
+        for (int i = 0; i < n; i++) {
+            aa[i] = sc.nextInt();
+            sum1 += aa[i];
+        }
+        final long x = sc.nextLong();
+
+        long d = x / sum1;
+
+        long sum = d * sum1;
+        int index = 0;
+        while (sum <= x) {
+            sum += aa[index];
+            index++;
+        }
+
+        long ans = d * aa.length + index;
+
+        System.out.println(ans);
+    }
+//}
+
+    @Test
+    public void Case1() {
+
+        String input = """
+                       3
+                       3 5 2
+                       26
+                    """;
+
+        String expected = """
+                          8
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC220C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case2() {
+
+        String input = """
+                       4
+                       12 34 56 78
+                       1000
+                    """;
+
+        String expected = """
+                          23
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC220C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+
+    // @Test
+    public void Case3() {
+
+        String input = """
+                       
+                    """;
+
+        String expected = """
+                          
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC220C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+}
