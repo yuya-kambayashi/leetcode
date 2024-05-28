3961a207f1dbc78c257fd049d7407319e3e2221c
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-05-28 17:12:36 +0900

    add

---
+++ src/test/java/yuya/kambayashi/ABC121C.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yuya.kambayashi;
+
+import java.util.*;
+import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ *
+ * @author kamba
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ABC121C {
+
+    private StandardInputSnatcher in = new StandardInputSnatcher();
+    private StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeAll
+    public void beforeAll() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+//import java.math.*;
+//import java.util.*;
+//import java.util.stream.*;
+//public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        List<Pair> pairs = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            pairs.add(new Pair(sc.nextLong(), sc.nextLong()));
+        }
+        Collections.sort(pairs, Comparator.comparingLong(Pair::getFirst));
+
+        long bill = 0;
+        for (int i = 0; i < n; i++) {
+            Pair p = pairs.get(i);
+            if (p.second < m) {
+                bill += p.first * p.second;
+                m -= p.second;
+            } else {
+                bill += p.first * m;
+                break;
+            }
+        }
+
+        System.out.println(bill);
+    }
+
+    static class Pair {
+
+        long first, second;
+
+        Pair(long first, long second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public long getFirst() {
+            return first;
+        }
+    }
+//}
+
+    @Test
+    public void Case1() {
+
+        String input = """
+                       2 5
+4 9
+2 4
+                    """;
+
+        String expected = """
+                          12
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC121C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case2() {
+
+        String input = """
+                       4 30
+6 18
+2 5
+3 10
+7 9
+                    """;
+
+        String expected = """
+                          130
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC121C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case3() {
+
+        String input = """
+                       1 100000
+1000000000 100000
+                    """;
+
+        String expected = """
+                          100000000000000
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC121C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine()).isEqualTo(s));
+    }
+}
