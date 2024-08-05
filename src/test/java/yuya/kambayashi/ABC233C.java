0264d9e2b1c85184fe29ff8180d257ae447af4b8
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-08-05 11:14:19 +0900

    add

---
+++ src/test/java/yuya/kambayashi/ABC233C.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yuya.kambayashi;
+
+import java.util.*;
+import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ *
+ * @author kamba
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ABC233C {
+
+    private StandardInputSnatcher in = new StandardInputSnatcher();
+    private StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeAll
+    public void beforeAll() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+//import java.math.*;
+//import java.util.*;
+//import java.util.stream.*;
+//public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int n = sc.nextInt();
+
+        System.out.println();
+    }
+//}
+
+    @Test
+    public void Case1() {
+
+        String input = """
+                       2 40
+3 1 8 4
+2 10 5
+                    """;
+
+        String expected = """
+                          2
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC233C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    //  @Test
+    public void Case2() {
+
+        String input = """
+                       3 200
+3 10 10 10
+3 10 10 10
+5 2 2 2 2 2
+                    """;
+
+        String expected = """
+                          45
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC233C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    // @Test
+    public void Case3() {
+
+        String input = """
+                       3 1000000000000000000
+2 1000000000 1000000000
+2 1000000000 1000000000
+2 1000000000 1000000000
+                    """;
+
+        String expected = """
+                          0
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC233C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    // @Test
+    public void Case4() {
+
+        String input = """
+                       
+                    """;
+
+        String expected = """
+                          
+                          """;
+        Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
+        ABC233C.main(null);
+        Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+}
