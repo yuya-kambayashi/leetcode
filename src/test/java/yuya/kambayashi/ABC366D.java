d78318d3566b3861689a0cfe137ee4062bb79bce
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-08-10 20:47:08 +0900

    add

--- src/test/java/yuya/kambayashi/ABC366D.java
+++ src/test/java/yuya/kambayashi/ABC366D.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.TestInstance;
  * @author kamba
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class ABC233C {
+public class ABC366D {
 
     private StandardInputSnatcher in = new StandardInputSnatcher();
     private StandardOutputSnatcher out = new StandardOutputSnatcher();
@@ -37,45 +37,13 @@ public class ABC233C {
 //import java.util.*;
 //import java.util.stream.*;
 //public class Main {
-    static long n;
-    static long x;
-    static long ans;
-    static List<List<Integer>> aaa;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextLong();
-        x = sc.nextLong();
-        ans = 0;
-        aaa = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int l = sc.nextInt();
-            List<Integer> aa = new ArrayList<>();
-            for (int j = 0; j < l; j++) {
-                int a = sc.nextInt();
-                if (x % a == 0) {
-                    aa.add(a);
-                }
-            }
-            aaa.add(aa);
-        }
-        dfs(0, 1);
-        System.out.println(ans);
-    }
+        final int n = sc.nextInt();
 
-    static void dfs(int pos, long product) {
-        if (pos == n) {
-            if (product == x) {
-                ans++;
-            }
-            return;
-        }
-        for (var a : aaa.get(pos)) {
-            if (a * product <= x) {
-                dfs(pos + 1, a * product);
-            }
-        }
+        System.out.println();
     }
 //}
 
@@ -83,52 +51,44 @@ public class ABC233C {
     public void Case1() {
 
         String input = """
-                       2 40
-3 1 8 4
-2 10 5
+                       
                     """;
 
         String expected = """
-                          2
+                          
                           """;
         Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
-        ABC233C.main(null);
+        ABC366D.main(null);
         Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    @Test
+    //  @Test
     public void Case2() {
 
         String input = """
-                       3 200
-3 10 10 10
-3 10 10 10
-5 2 2 2 2 2
+                       
                     """;
 
         String expected = """
-                          45
+                          
                           """;
         Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
-        ABC233C.main(null);
+        ABC366D.main(null);
         Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    @Test
+    // @Test
     public void Case3() {
 
         String input = """
-                       3 1000000000000000000
-2 1000000000 1000000000
-2 1000000000 1000000000
-2 1000000000 1000000000
+                       
                     """;
 
         String expected = """
-                          0
+                          
                           """;
         Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
-        ABC233C.main(null);
+        ABC366D.main(null);
         Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
@@ -143,7 +103,7 @@ public class ABC233C {
                           
                           """;
         Stream.of(input.split("\\n")).map(s -> s.trim()).forEach(s -> in.inputln(s));
-        ABC233C.main(null);
+        ABC366D.main(null);
         Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 }
