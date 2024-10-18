4b39e69f777a305d6747e8025fe3a5c8156e7655
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-10-18 21:10:08 +0900

    add

--- src/test/java/yuya/kambayashi/ABC216D.java
+++ src/test/java/yuya/kambayashi/ABC216D.java
@@ -38,12 +38,45 @@ public class ABC216D {
 //import java.util.stream.*;
 //public class Main {
 
+    //https://atcoder.jp/contests/abc216/submissions/25459428
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
-
-        System.out.println();
+        final int m = sc.nextInt();
+        ArrayList<Integer>[] vs = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            vs[i] = new ArrayList<>();
+        }
+        int[] deg = new int[n];
+        for (int i = 0; i < m; i++) {
+            int k = sc.nextInt();
+            int p = sc.nextInt() - 1;
+            for (int j = 1; j < k; j++) {
+                int q = sc.nextInt() - 1;
+                vs[p].add(q);
+                deg[q]++;
+                p = q;
+            }
+        }
+        Queue<Integer> que = new LinkedList<>();
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            if (deg[i] == 0) {
+                que.add(i);
+            }
+        }
+        while (!que.isEmpty()) {
+            int x = que.poll();
+            k++;
+            for (int y : vs[x]) {
+                deg[y]--;
+                if (deg[y] == 0) {
+                    que.add(y);
+                }
+            }
+        }
+        System.out.println(k == n ? "Yes" : "No");
     }
 //}
 
@@ -66,7 +99,7 @@ public class ABC216D {
         Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //  @Test
+    @Test
     public void Case2() {
 
         String input = """
@@ -99,7 +132,7 @@ public class ABC216D {
         ABC216D.main(null);
         Stream.of(expected.split("\\n")).map(s -> s.trim()).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
-    
+
     // @Test
     public void Case4() {
 
