5f060acf9800eadf6b3416abccb174d0463c17c8
KAMBAYASHI Yuya <kambayashi73@gmail.com>
2025-05-21 20:58:28 +0900

    Update LC011.java

--- src/main/java/yuya/kambayashi/LC011.java
+++ src/main/java/yuya/kambayashi/LC011.java
@@ -7,7 +7,7 @@ public class LC011 {
         int left = 0;
         int right = height.length - 1;
 
-        while(left < right){
+        while(left < right){　
             int width = right - left;
 
             maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * width);
